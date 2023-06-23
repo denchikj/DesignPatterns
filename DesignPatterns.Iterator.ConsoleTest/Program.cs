adeedeb9c0d6f220d8a2eddf10978db04ab15450
denchikj <igoshevdenis9876@gmail.com>
2023-06-23 18:39:33 +0500

    Iterator

---
+++ DesignPatterns.Iterator.ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Iterator.ConsoleTest
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var iterator = new Implementation.Iterator(collection);
+            
+            Console.WriteLine("pass 1:");
+            while (iterator.MoveNext())
+            {
+                Console.Write(iterator.Current + " ");
+            }
+
+            iterator.Reset();
+            
+            Console.WriteLine("\npass 2 after reset:");
+            while (iterator.MoveNext())
+            {
+                Console.Write(iterator.Current + " ");
+            }
+        }
+    }
+}
---
+++ DesignPatterns.Iterator/Iterator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace DesignPatterns.Iterator.Implementation
+{
+    public class Iterator : IEnumerator
+    {
+        private readonly int[] _collection;
+        private int _index = -1;
+
+        public Iterator(int[] collection)
+        {
+            _collection = collection;
+        }
+
+        public object Current => GetCurrent();
+
+        private object GetCurrent() => _collection[_index];
+
+        public bool MoveNext()
+        {
+            _index++;
+            return _index < _collection.Length;
+        }
+
+        public void Reset() => _index = -1;
+    }
+}
