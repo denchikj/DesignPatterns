7b8d63bb7e82eccfae2a5af40cd1091dfe946e5d
denchikj <igoshevdenis9876@gmail.com>
2023-06-23 19:48:42 +0500

    ChainOfResponsibility

---
+++ DesignPatterns.ChainOfResponsibility.ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.ChainOfResponsibility.Handlers;
+using DesignPatterns.ChainOfResponsibility.Interfaces;
+using DesignPatterns.ChainOfResponsibility.Requests;
+
+namespace DesignPatterns.ChainOfResponsibility.ConsoleTest
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            IChain chain = new Chain.Chain();
+            chain.AddNext(new PutHandler());
+            chain.AddNext(new PostHandler());
+            chain.AddNext(new GetHandler());
+            chain.Handle(new PutRequest(3));
+        }
+    }
+}
